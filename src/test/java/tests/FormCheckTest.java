cc2398aabdb9d079db3f92520e9fcee991feec55
LSKozarina <lubov.kozarina@pix.ru>
2025-03-02 20:47:18 +0300

    Added FormCheckTest

---
+++ src/test/java/tests/FormCheckTest.java
@@ -0,0 +1,74 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class FormCheckTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 5000;
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Ivan");
+        $("#lastName").setValue("Ivanov");
+        $("#userEmail").setValue("ivan@ivanov.com");
+        $$("label").findBy(text("Male")).click();
+        $("#userNumber").setValue("1234567890");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("February");
+        $(".react-datepicker__year-select").selectOption("2000");
+        $(".react-datepicker__day--028").click();
+        $("#subjectsInput").setValue("Ma").pressEnter();
+        $$("label").findBy(text("Music")).click();
+        $("#uploadPicture").uploadFile(new File("C:\\Users\\Любовь\\Documents\\Обучение\\test.jpg"));
+        $("#currentAddress").setValue("Some street 1");
+        $("#state").scrollIntoView(true);
+        $("#state").click();
+        $(byText("Haryana")).click();
+        $("#city").click();
+        $(byText("Karnal")).click();
+        $("#submit").scrollIntoView(true);
+        $("#submit").click();
+
+
+
+        $(".modal-content").shouldBe(visible);
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-content").shouldHave(text("Ivan" + " " + "Ivanov"));
+        $(".modal-content").shouldHave(text("ivan@ivanov.com"));
+        $(".modal-content").shouldHave(text("Male"));
+        $(".modal-content").shouldHave(text("1234567890"));
+        $(".modal-content").shouldHave(text("28" + " " + "February" + "," + "2000"));
+        $(".modal-content").shouldHave(text("Maths"));
+        $(".modal-content").shouldHave(text("Music"));
+        $(".modal-content").shouldHave(text("test.jpg"));
+        $(".modal-content").shouldHave(text("Some street 1"));
+        $(".modal-content").shouldHave(text("Haryana" + " " + "Karnal"));
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
