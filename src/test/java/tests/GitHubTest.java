70dd8ed427f6a44e35595c594e3cbfc41db82bd1
LSKozarina <lubov.kozarina@pix.ru>
2025-03-11 18:58:38 +0300

    added GitHubTest

---
+++ src/test/java/tests/GitHubTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GitHubTest {
+    @Test
+    void shouldOpenEnterprizePage() {
+
+        open("https://github.com/");
+        $$("ul li").findBy(text("Solutions")).hover();
+        $$("ul li a").findBy(text("Enterprises")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered\n" +
+                "developer platform"
+        ));
+    }
+}
