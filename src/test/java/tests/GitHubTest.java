2c2b09cd0d4e60cee358b5468f88fb28981d39fc
LSKozarina <lubov.kozarina@pix.ru>
2025-03-11 19:05:11 +0300

    removed extra lines

--- src/test/java/tests/GitHubTest.java
+++ src/test/java/tests/GitHubTest.java
@@ -5,14 +5,12 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GitHubTest {
-    @Test
 
+    @Test
     void shouldOpenEnterprisePage() {
         open("https://github.com/");
         $$("ul li").findBy(text("Solutions")).hover();
         $$("ul li a").findBy(text("Enterprises")).click();
-        $("#hero-section-brand-heading").shouldHave(text("The AI-powered\n" +
-                "developer platform"
-        ));
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered\n" + "developer platform"));
     }
 }
