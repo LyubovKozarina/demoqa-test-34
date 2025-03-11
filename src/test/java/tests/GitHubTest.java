191b562f1d339aad469522e2bdcf2dd56192e4c1
LSKozarina <lubov.kozarina@pix.ru>
2025-03-11 19:03:00 +0300

    removed extra lines

--- src/test/java/tests/GitHubTest.java
+++ src/test/java/tests/GitHubTest.java
@@ -3,7 +3,6 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GitHubTest {
