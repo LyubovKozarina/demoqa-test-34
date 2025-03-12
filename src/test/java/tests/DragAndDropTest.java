e8ebd6efc6b15ab44bdc1c3c5526ac1cf9ab7e70
LSKozarina <lubov.kozarina@pix.ru>
2025-03-12 13:26:16 +0300

    added DragAndDropTest

---
+++ src/test/java/tests/DragAndDropTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.interactions.Actions;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+
+public class DragAndDropTest {
+
+    @Test
+    void shouldMovingRectangleTest() {
+        Selenide.open("https://the-internet.herokuapp.com/drag_and_drop");
+        Actions actions = Selenide.actions();
+        actions.dragAndDrop($("#column-a").toWebElement(), $("#column-b").toWebElement()).perform();
+        //$("#column-a").dragAndDropTo($("#column-b")); //не работает
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
+
