95b756faf9c9fb3c24a167ef409d37e4e8fac4ad
LSKozarina <lubov.kozarina@pix.ru>
2025-04-10 22:24:09 +0300

    added params

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -5,8 +5,6 @@ import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.selenide.Selenide.sessionId;
@@ -38,7 +36,11 @@ public class Attach {
 
     @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
     public static String addVideo() {
-        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
+        String sessionId = sessionId().toString();
+
+        String videoBaseUrl = System.getProperty("videoUrl", "");
+        String videoUrl = videoBaseUrl + "/" + sessionId + ".mp4";
+
         return "<html><body><video width='100%' height='100%' controls autoplay>" +
                 "<source src='" + videoUrl + "' type='video/mp4'>" +
                 "</video></body></html>";
--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -8,20 +8,37 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class TestBase {
     @BeforeAll
     static void setup() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 5000;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        String browser = System.getProperty("browser", "chrome");
+        String browserVersion = System.getProperty("browserVersion", "120.0");
+        String remoteUrl = System.getProperty("remoteUrl", "");
+        String screenResolution = System.getProperty("screenResolution", "");
+
+        Configuration.browser = browser;
+        Configuration.browserVersion = browserVersion;
+
+        if (!remoteUrl.isBlank()) {
+            Configuration.remote = remoteUrl;
+        }
+
+        Map<String, Object> selenoidOptions = new HashMap<>();
+        selenoidOptions.put("enableVNC", true);
+        selenoidOptions.put("enableVideo", true);
+
+        if (!screenResolution.isBlank()) {
+            selenoidOptions.put("screenResolution", screenResolution);
+        }
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
+        capabilities.setCapability("selenoid:options", selenoidOptions);
         Configuration.browserCapabilities = capabilities;
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
