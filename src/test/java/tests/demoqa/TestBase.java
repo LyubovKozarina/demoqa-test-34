9357ac78b772b0b732c5b8bf7c9359eedec7e07a
LSKozarina <lubov.kozarina@pix.ru>
2025-04-15 23:47:26 +0300

    удалены промежуточные переменные, Allure listener перенесен в @BeforeEach

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -6,41 +6,39 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class TestBase {
+
     @BeforeAll
     static void setup() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.timeout = 5000;
 
-        String browser = System.getProperty("browser", "chrome");
-        String browserVersion = System.getProperty("browserVersion", "120.0");
-        String remoteUrl = System.getProperty("remoteUrl", "");
-        String screenResolution = System.getProperty("screenResolution", "");
-
-        Configuration.browser = browser;
-        Configuration.browserVersion = browserVersion;
-
-        if (!remoteUrl.isBlank()) {
-            Configuration.remote = remoteUrl;
-        }
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "125.0");
+        Configuration.remote = System.getProperty("remoteUrl", "");
 
         Map<String, Object> selenoidOptions = new HashMap<>();
         selenoidOptions.put("enableVNC", true);
         selenoidOptions.put("enableVideo", true);
 
-        if (!screenResolution.isBlank()) {
+        String screenResolution = System.getProperty("screenResolution");
+        if (screenResolution != null && !screenResolution.isBlank()) {
             selenoidOptions.put("screenResolution", screenResolution);
         }
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", selenoidOptions);
         Configuration.browserCapabilities = capabilities;
+    }
 
+    @BeforeEach
+    void addAllureListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
